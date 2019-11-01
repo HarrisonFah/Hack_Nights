d5a209349d6b444d18de9785b56451887a6aa17a
danivi2000 <57239562+danivi2000@users.noreply.github.com>
2019-11-01 09:58:18 -0600

    Update version3.py
    I modifying the move function, it is now called with a string arguement x or y either positive or negative to dictate direction
    i also edited the gravity function, it takes in the strength of gravity as an arguement, changing this will change the height of the jump/sensitivity

--- version3.py
+++ version3.py
@@ -21,7 +21,7 @@ class Game:
         self.frame_rate = 60
         self.surface = surface
         self.background_color = pygame.Color('black')
-        self.wall_1 = wall('orange',[450, 150],[20, 100], self.surface)
+        self.wall_1 = wall('orange',[450, 150],[20, 100], [20,20], self.surface)
         p = pyaudio.PyAudio()
         self.stream = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,frames_per_buffer=CHUNK)
         data = np.frombuffer(self.stream.read(CHUNK),dtype=np.int16)
@@ -62,9 +62,8 @@ class Game:
         pygame.display.update() #displays updated surface
        
     def update(self,jump):
-        self.wall_1.apply_gravity()
         if jump == True:
-            self.wall_1.move([0,-20])
+            self.wall_1.move('-y')
         pass
     
     def decide_continue(self):
@@ -75,36 +74,45 @@ class Game:
         for event in events:
             if event.type == pygame.QUIT:
                 self.close_clicked = True
-            if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.wall_1.move([0,20])   
+            #if event.type == pygame.KEYDOWN:
+            #        if event.key == pygame.K_UP:
+            #            self.wall_1.move('-y')   
         keys = pygame.key.get_pressed()  
         if keys [pygame.K_UP]:
-            self.wall_1.move([0,-20])
+            self.wall_1.move('-y')
         if keys [pygame.K_DOWN]:
-            self.wall_1.move([0,20])
+            self.wall_1.move('y')
         if keys [pygame.K_LEFT]:
-            self.wall_1.move([-20,0])
+            self.wall_1.move('-x')
         if keys [pygame.K_RIGHT]:
-            self.wall_1.move([20,0])
+            self.wall_1.move('x')
         else:
-            self.wall_1.move([0,0])
-            self.wall_1.apply_gravity()
+            self.wall_1.apply_gravity(1.81)
                 
 class wall:
-    def __init__(self, wall_colour, wall_position, wall_dimensions, surface):
+    def __init__(self, wall_colour, wall_position, wall_dimensions, velocity, surface):
         self.color = pygame.Color(wall_colour)
         self.position = wall_position
         self.dimensions = wall_dimensions
         self.wall = [wall_position, wall_dimensions]
         self.surface = surface
-    def move(self, velocity):
         self.velocity = velocity 
-
-        self.position[0] = self.position[0] + self.velocity[0]
-        self.position[1] = self.position[1] + self.velocity[1]
+    def move(self, direction):
+        # note that direction must be either x or y positive or nagative
+        self.direction = direction
         
+        if self.direction == '-x':
+            self.position[0] = self.position[0] - self.velocity[0]
+        if self.direction == 'x':
+            self.position[0] = self.position[0] + self.velocity[0]
+        if self.direction == '-y':
+            self.position[1] = self.position[1] - self.velocity[1]
+        if self.direction == 'y':
+            self.position[1] = self.position[1] + self.velocity[1]
+            
+ 
         size = self.surface.get_size() # tuple (width, height)
+
         # check right index = 0, check bottom index = 1
         for index in range(0,2):
             if self.position[index] + self.dimensions[index] >= size[index]:
@@ -120,17 +128,17 @@ class wall:
     def draw(self):
         pygame.draw.rect(self.surface, self.color, self.wall)
     
-    def apply_gravity(self):
-        pass
-        '''size = self.surface.get_size() # tuple (width, height)
-        change_in_velocity = 0.50
-        if self.velocity[1] == 0:
-            self.move([0,19])
-            self.velocity[1] += change_in_velocity
+    def apply_gravity(self, gravitation):
+        self.gravity_strength = gravitation
+        size = self.surface.get_size() # tuple (width, height)
+
+        self.position[1] = self.position[1] + 0.5*self.velocity[1]
+        self.position[1] += self.gravity_strength
+
         # check if on ground
-        if self.position[1] - self.dimensions[1] >= size[1]:
-            #self.position[1] = size[1] - self.dimensions[1]
-            self.velocity[1] = 0'''
+        if self.position[1] + self.dimensions[1] >= size[1]:
+            self.position[1] = size[1] - self.dimensions[1]
+            #self.velocity[1] = 0
         
 
 def main():
