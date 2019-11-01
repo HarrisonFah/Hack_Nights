67ee1a604b3e30664373d9720cbe9a40de59be85
danivi2000 <57239562+danivi2000@users.noreply.github.com>
2019-10-31 18:53:35 -0600

    Update version2.py

--- version2.py
+++ version2.py
@@ -6,6 +6,12 @@ Created on Wed Oct 23 14:20:14 2019
 """
 
 import pygame
+import numpy as np
+import time
+import pyaudio
+
+CHUNK = 4096
+RATE = 44100
 
 class Game:
     def __init__(self,surface):
@@ -15,11 +21,19 @@ class Game:
         self.frame_rate = 60
         self.surface = surface
         self.background_color = pygame.Color('black')
-   
         self.wall_1 = wall('orange',[450, 150],[20, 100], self.surface)
-    
+        p = pyaudio.PyaAdio()
+        self.stream = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,frame_per_buffer=CHUNK)
+        sum = 0
+        count = 0
+        for i in range(len(data)):
+            sum = sum + data[i]
+            count = count + 1
+        self.baseline = sum/count
     def play(self):
         while self.close_clicked == False:
+            data = np.fromstring(self.stream.read(CHUNK),dtype=np.int16)
+            jump = jump_or_not(data)
             self.update
             self.draw()
             self.handle_events()
@@ -27,7 +41,17 @@ class Game:
 
                 self.decide_continue
             self.game_clock.tick(self.frame_rate)
-    
+    def jump_or_not(self,data):
+        count = 0
+        sum = 0
+        for i in range(len(data)):
+            sum = sum + data[i]
+            count = count + 1
+        mean = sum/count
+        if mean > self.baseline:
+            return True
+        else:
+            return False
     def draw(self):
         # Draw all game objects
         self.surface.fill(self.background_color) #clear display surface first
@@ -37,6 +61,8 @@ class Game:
        
     def update(self):
         self.wall.apply_gravity()
+        if jump == True:
+            self.wall_1.move([0,-20])
         pass
     
     def decide_continue(self):
@@ -85,7 +111,10 @@ class wall:
         for index in range(0,2):
             if self.position[index] <= 0:
                 self.position[index] = 0
-        
+    
+    def jump(self):
+        pass
+    
     def draw(self):
         pygame.draw.rect(self.surface, self.color, self.wall)
     
